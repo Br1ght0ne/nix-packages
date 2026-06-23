661b84da529e84007c6027b7c8ff8d41109e2c4f
Oleksii Filonenko <github@brightone.cloud>
2026-06-23 13:29:34 +0100

    cataclysm-dda-git-bin: 2026-06-17-22 -> 2026-06-23-0742

--- packages/cataclysm/cataclysm-dda-git-bin/default.nix
+++ packages/cataclysm/cataclysm-dda-git-bin/default.nix
@@ -1,7 +1,7 @@
 { callPackage }:
 (callPackage ../common.nix { }).makeCddaPackage rec {
   pname = "cataclysm-dda-bin-git";
-  timestamp = "2026-06-17-2219";
+  timestamp = "2026-06-23-0742";
   version = "cdda-experimental-${timestamp}";
-  hash = "sha256-Zb/e2GlgVxx6IlRd80OXLaKMW2eqrDiOtvoNnlCJ96c=";
+  hash = "sha256-pZSXytS3+Nga/c/lObmP7PUqXBcyOS9Ym/7xrzo9BbQ=";
 }
